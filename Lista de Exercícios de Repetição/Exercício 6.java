aff20f1f5342718662ad2a3565a58ce5d4bbe396
Tales Costa <talonacontato@gmail.com>
2024-04-06 02:41:18 -0300

    s

--- Lista de Exercícios de Repetição/Exercício 6.java
+++ Lista de Exercícios de Repetição/Exercício 6.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         int soma = 0;
-        int media; 
-        
+        int media;
+
         for (int i = 0; i < 5; i++) {
             System.out.print("Digite o número " + (i + 1) + ": ");
             int numero = scanner.nextInt();
-            
-            soma += numero; 
-        
+
+            soma += numero;
+        }
+
         media = soma / 5;
-        
+
         System.out.println("A soma dos números é: " + soma);
         System.out.println("A média dos números é: " + media);
-        
+
         scanner.close();
     }
 }
